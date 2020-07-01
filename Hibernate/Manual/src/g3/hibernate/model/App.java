fa7469862fb099a9cb7df87d40f9b5ac6918a26c
Srinimaddy <66350330+Srinimaddy@users.noreply.github.com>
2020-07-02 00:23:20 +0530

    Add files via upload

---
+++ Hibernate/Manual/src/g3/hibernate/model/App.java
@@ -0,0 +1,23 @@
+package g3.hibernate.model;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		User user = new User();
+		user.setId(2);
+		user.setName("Srini");
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();//session connection
+		session.beginTransaction();//Ready for query
+		session.save(user);//API
+		session.getTransaction().commit();//Will affect the Backend
+	
+
+	}
+
+}
